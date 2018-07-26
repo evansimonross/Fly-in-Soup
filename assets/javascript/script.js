0a467e3ed89ba5b40682d244137fd0b6ff92b219
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-07-25 22:05:18 -0400

    Added a quick request from the health dept api. Parameters will likely need to change in the future.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,10 @@
-$(function() {
-    
-})
+// Search a restaurant by name in the health inspection database. 
+function searchRestaurant(restaurantName){
+    var queryUrl = "https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=" + restaurantName.toUpperCase().replace(" ", "+");
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+}
