c4f21adef3be081aadd3e109aeaec37df47b11a6
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-07-30 19:07:57 -0400

    Changed the modal a bit.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,9 +26,9 @@ $(function() {
         $(`#${num}`).append($(`<h5 class="card-title">${obj.dba}</h5>`))
         $(`#${num}`).append($(`<h6 class="add1 card-subtitle mb-2 text-muted"> ${obj.address1}</h6>`))
         $(`#${num}`).append($(`<h6 class="add2 card-subtitle mb-2 text-muted"> ${obj.address2}</h6>`))
-        $(`#${num}`).append($(`<p class="v-icon card-text"> Violation Code: ${obj.violation_code}</p>`))
         $(`#${num}`).attr('data-record-date', obj["record_date"])
         $(`#${num}`).attr('data-inspection-date', obj["inspection_date"])
+        $(`#${num}`).attr('data-violation-code', obj["violation_code"])
         $(`#${num}`).attr('data-violation-description', obj["violation_description"])
         $(`#${num}`).attr('data-cuisine', obj["cuisine_description"])
 
@@ -99,7 +99,6 @@ $(function() {
         let resturantName = $(this).find(".card-title").text()
         let fullAddress = $(this).find(".add1").text() + " " + $(this).find(".add2").text()
         let grade = $(this).find(".mini-grade").attr('src');
-        let vcode = $(this).find(".v-icon").text()
 
         let index = allData.findIndex(x => x.name === resturantName)
         
@@ -108,16 +107,14 @@ $(function() {
         
         $(".modal-body").html($(`<div class="text-center"><img id="modal-grade" src="${grade}" alt="map"></div>`))
         $(".modal-body").append($(`<h6 class="text-center text-muted">${fullAddress}</h6>`))
-        $(".modal-body").append($(`<div class="row modal-row"><div class="col-md-6"><p><strong>Grade:</strong> ${grade}</p></div><div class="col-md-6"><p><strong>Violation Code:</strong> ${vcode}</p></div></div>`))
         
         // more info part
-        $(".modal-body").append($("<h5>").text("Additional Information"))
+        var card = $(this).find(".card-body")
         $(".modal-body").append($("<ul>").addClass("more-info"))
-        
-        $(".more-info").append($("<li>").text("Cuisine: " + $(this).find(".card-body").attr("data-cuisine")))
-        $(".more-info").append($("<li>").text("Record Date: " + $(this).find(".card-body").attr("data-record-date")))
-        $(".more-info").append($("<li>").text("Inspection Date: " + $(this).find(".card-body").attr("data-inspection-date")))
-        $(".more-info").append($("<li>").text("Violation Description: " + $(this).find(".card-body").attr("data-violation-description")))
+        $(".more-info").append($(`<li><b>Cuisine</b>: ${card.attr("data-cuisine")}</li>`))
+        $(".more-info").append($(`<li><b>Violation Code</b>: ${card.attr("data-violation-code")}</li>`))
+        $(".more-info").append($(`<li><b>Violation Description</b>: ${card.attr("data-violation-description")}</li>`))
+        $(".more-info").append($(`<li><b>Inspection Date</b>: ${card.attr("data-inspection-date")}</li>`))
 
         $('#myModal').modal('show')
 
